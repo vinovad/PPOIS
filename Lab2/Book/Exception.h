3fa1084dde8d8349328e2dc44a6edccfa0a50ea7
vinovad <advaskan@gmail.com>
2025-10-10 13:29:11 +0300

    Exception.h

---
+++ Lab2/Book/Exception.h
@@ -0,0 +1,30 @@
+'''cpp
+  /**
+ * @file Exception.h
+ * @brief Класс исключений для системы управления книгами
+ */
+
+#ifndef EXCEPTION_H
+#define EXCEPTION_H
+
+#include <stdexcept>
+#include <string>
+using namespace std;
+
+/**
+ * @class Exception
+ * @brief Пользовательский класс исключений
+ * 
+ * Наследует от std::runtime_error для обработки ошибок в системе.
+ */
+class Exception : public runtime_error {
+public:
+    /**
+     * @brief Конструктор исключения
+     * @param msg Сообщение об ошибке
+     */
+    Exception(const string& msg) : runtime_error(msg) {}
+};
+
+#endif
+  '''
