d2546782491e995df9475d1e8950467e7a2db630
vinovad <advaskan@gmail.com>
2025-10-10 13:32:59 +0300

    Create YearOfManufacture.h

---
+++ Lab2/Book/Class/YearOfManufacture.h
@@ -0,0 +1,54 @@
+''cpp
+
+  /**
+ * @file YearOfManufacture.h
+ * @brief Класс для работы с годом издания книги
+ */
+
+#ifndef YEAROFMANUFACTURE_H
+#define YEAROFMANUFACTURE_H
+
+#include <string>
+using namespace std;
+
+/**
+ * @class YearOfManufacture
+ * @brief Представляет год издания книги
+ */
+class YearOfManufacture {
+private:
+    int year; ///< Год издания
+
+public:
+    /**
+     * @brief Конструктор с параметром
+     * @param y Год издания
+     */
+    YearOfManufacture(int y);
+    
+    /**
+     * @brief Конструктор по умолчанию
+     */
+    YearOfManufacture();
+    
+    /**
+     * @brief Получить год издания
+     * @return Год издания
+     */
+    int get_Year();
+    
+    /**
+     * @brief Установить год издания (целое число)
+     * @param y Новый год издания
+     */
+    void set_Year(int y);
+    
+    /**
+     * @brief Установить год издания (строка)
+     * @param g Год издания в виде строки
+     */
+    void set_Year(string g);
+};
+
+#endif
+  '''
