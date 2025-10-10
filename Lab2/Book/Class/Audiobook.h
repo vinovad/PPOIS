71b80718442817b322db6b88def7502daa683fbc
vinovad <advaskan@gmail.com>
2025-10-10 13:50:07 +0300

    Create Audiobook.h

---
+++ Lab2/Book/Class/Audiobook.h
@@ -0,0 +1,38 @@
+'''cpp
+/**
+ * @file Audiobook.h
+ * @brief Класс для работы с аудиокнигами
+ */
+
+#ifndef AUDIOBOOK_H
+#define AUDIOBOOK_H
+
+/**
+ * @class Audiobook
+ * @brief Представляет информацию о наличии аудиоверсии книги
+ */
+class Audiobook {
+private:
+    bool has_audiobook; ///< Наличие аудиокниги
+
+public:
+    /**
+     * @brief Конструктор с параметром
+     * @param audio Наличие аудиокниги
+     */
+    Audiobook(bool audio);
+    
+    /**
+     * @brief Конструктор по умолчанию
+     */
+    Audiobook();
+    
+    /**
+     * @brief Проверить наличие аудиоверсии
+     * @return true если аудиокнига доступна, false в противном случае
+     */
+    bool has_audiobook_version();
+};
+
+#endif
+'''
