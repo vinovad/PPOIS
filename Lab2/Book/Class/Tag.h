6c1cb0f7687c8f9d5381b7541ab84ecef99f8e44
vinovad <advaskan@gmail.com>
2025-10-10 13:30:45 +0300

    Create Tag.h

---
+++ Lab2/Book/Class/Tag.h
@@ -0,0 +1,47 @@
+'''cpp
+/**
+ * @file Tag.h
+ * @brief Класс для работы с тегами книг
+ */
+
+#ifndef TAG_H
+#define TAG_H
+
+#include <string>
+using namespace std;
+
+/**
+ * @class Tag
+ * @brief Представляет тег для категоризации книг
+ */
+class Tag {
+private:
+    string book_tag; ///< Название тега
+
+public:
+    /**
+     * @brief Конструктор с параметром
+     * @param t Название тега
+     */
+    Tag(const string& t);
+    
+    /**
+     * @brief Конструктор по умолчанию
+     */
+    Tag();
+    
+    /**
+     * @brief Получить тег
+     * @return Название тега
+     */
+    string get_Tag();
+    
+    /**
+     * @brief Установить тег
+     * @param tg Новое название тега
+     */
+    void set_Tag(string tg);
+};
+
+#endif
+'''
