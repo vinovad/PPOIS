b9837bb7ef3948a334ce561a3b71a95e29698342
vinovad <advaskan@gmail.com>
2025-10-14 18:50:35 +0300

    Update Exception.h

--- Lab2/Book/Class/Exception.h
+++ Lab2/Book/Class/Exception.h
@@ -1,7 +1,6 @@
-
-  /**
+/**
  * @file Exception.h
- * @brief Класс исключений для системы управления книгами
+ * @brief Классы исключений для системы управления книгами
  */
 
 #ifndef EXCEPTION_H
@@ -12,19 +11,110 @@
 using namespace std;
 
 /**
- * @class Exception
- * @brief Пользовательский класс исключений
- * 
- * Наследует от std::runtime_error для обработки ошибок в системе.
+ * @class TagException
+ * @brief Исключение для работы с тегами
  */
-class Exception : public runtime_error {
+class TagException : public runtime_error {
 public:
-    /**
-     * @brief Конструктор исключения
-     * @param msg Сообщение об ошибке
-     */
-    Exception(const string& msg) : runtime_error(msg) {}
+    TagException(const string& msg) : runtime_error(msg) {}
+};
+
+/**
+ * @class GenreException
+ * @brief Исключение для работы с жанрами
+ */
+class GenreException : public runtime_error {
+public:
+    GenreException(const string& msg) : runtime_error(msg) {}
+};
+
+/**
+ * @class YearException
+ * @brief Исключение для работы с годами издания
+ */
+class YearException : public runtime_error {
+public:
+    YearException(const string& msg) : runtime_error(msg) {}
+};
+
+/**
+ * @class LanguageException
+ * @brief Исключение для работы с языками
+ */
+class LanguageException : public runtime_error {
+public:
+    LanguageException(const string& msg) : runtime_error(msg) {}
+};
+
+/**
+ * @class AuthorBirthException
+ * @brief Исключение для ошибок в дате рождения автора
+ */
+class AuthorBirthException : public runtime_error {
+public:
+    AuthorBirthException(const string& msg) : runtime_error(msg) {}
+};
+
+/**
+ * @class AuthorBooksException
+ * @brief Исключение для ошибок в количестве книг автора
+ */
+class AuthorBooksException : public runtime_error {
+public:
+    AuthorBooksException(const string& msg) : runtime_error(msg) {}
+};
+/**
+ * @class RatingException
+ * @brief Исключение для работы с рейтингами
+ */
+class RatingException : public runtime_error {
+public:
+    RatingException(const string& msg) : runtime_error(msg) {}
+};
+
+/**
+ * @class PublisherException
+ * @brief Исключение для работы с издательствами
+ */
+class PublisherException : public runtime_error {
+public:
+    PublisherException(const string& msg) : runtime_error(msg) {}
+};
+
+/**
+ * @class PageCountException
+ * @brief Исключение для работы с количеством страниц
+ */
+class PageCountException : public runtime_error {
+public:
+    PageCountException(const string& msg) : runtime_error(msg) {}
+};
+
+/**
+ * @class BookTitleException
+ * @brief Исключение для работы с названиями книг
+ */
+class BookTitleException : public runtime_error {
+public:
+    BookTitleException(const string& msg) : runtime_error(msg) {}
+};
+
+/**
+ * @class AgeRestrictionException
+ * @brief Исключение для работы с возрастными ограничениями
+ */
+class AgeRestrictionException : public runtime_error {
+public:
+    AgeRestrictionException(const string& msg) : runtime_error(msg) {}
+};
+
+/**
+ * @class CoverFormatException
+ * @brief Исключение для работы с форматами обложки
+ */
+class CoverFormatException : public runtime_error {
+public:
+    CoverFormatException(const string& msg) : runtime_error(msg) {}
 };
 
 #endif
- 
