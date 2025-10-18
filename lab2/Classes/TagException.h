6f0f720a0c29f186aaeaae8bd2f263fdb4ba5e97
vinovad <advaskan@gmail.com>
2025-10-18 10:17:22 +0300

    Update TagException.h

--- lab2/Classes/TagException.h
+++ lab2/Classes/TagException.h
@@ -1,18 +1,12 @@
 /**
  * @file TagException.h
- * @brief Исключение для работы с тегами
+ * @brief РСЃРєР»СЋС‡РµРЅРёРµ РґР»СЏ СЂР°Р±РѕС‚С‹ СЃ С‚РµРіР°РјРё
  */
-
 #ifndef TAG_EXCEPTION_H
 #define TAG_EXCEPTION_H
-
 #include <stdexcept>
 #include <string>
 using namespace std;
-
 class TagException : public runtime_error {
-public:
-    TagException(const string& msg) : runtime_error(msg) {}
-};
-
+public: TagException(const string& msg) : runtime_error(msg) {} };
 #endif
