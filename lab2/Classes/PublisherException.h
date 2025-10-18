66c36b4628623501f653afd6d97513c3eacab988
vinovad <advaskan@gmail.com>
2025-10-18 10:15:42 +0300

    Update PublisherException.h

--- lab2/Classes/PublisherException.h
+++ lab2/Classes/PublisherException.h
@@ -1,18 +1,13 @@
 /**
  * @file PublisherException.h
- * @brief Исключение для работы с издательствами
+ * @brief РСЃРєР»СЋС‡РµРЅРёРµ РґР»СЏ СЂР°Р±РѕС‚С‹ СЃ РёР·РґР°С‚РµР»СЊСЃС‚РІР°РјРё
  */
-
 #ifndef PUBLISHER_EXCEPTION_H
 #define PUBLISHER_EXCEPTION_H
-
 #include <stdexcept>
 #include <string>
 using namespace std;
-
 class PublisherException : public runtime_error {
-public:
-    PublisherException(const string& msg) : runtime_error(msg) {}
+public:    PublisherException(const string& msg) : runtime_error(msg) {}
 };
-
 #endif
