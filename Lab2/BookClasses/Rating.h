772187b510c14efcd494c6c6b1f6c6dcb4dbff87
vinovad <advaskan@gmail.com>
2025-11-20 15:57:23 +0300

    Update Rating.h

--- Lab2/BookClasses/Rating.h
+++ Lab2/BookClasses/Rating.h
@@ -1,44 +1,18 @@
-/**
- * @file Rating.h
- * @brief Класс для работы с рейтингом книги
- */
 
 #ifndef RATING_H
 #define RATING_H
-
- /**
-  * @class Rating
-  * @brief Представляет рейтинг книги и статистику оценок
-  */
 class Rating {
 private:
-    double rating;          ///< Текущий рейтинг
-    int number_of_votes;    ///< Количество голосов
-    double average_rating;  ///< Средний рейтинг
+    double rating;        
+    int number_of_votes;    
+    double average_rating; 
 
 public:
-    /**
-     * @brief Конструктор с параметром
-     * @param r Начальный рейтинг
-     */
     Rating(double r);
-
-    /**
-     * @brief Конструктор по умолчанию
-     */
     Rating();
-
-    /**
-     * @brief Получить текущий рейтинг
-     * @return Текущий рейтинг
-     */
     double get_rating();
-
-    /**
-     * @brief Установить рейтинг
-     * @param r Новый рейтинг
-     */
     void set_rating(double r);
 };
+
 
 #endif
