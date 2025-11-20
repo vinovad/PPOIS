61792e230a06a7e2df9e3cee165dd6e3e58b8114
vinovad <advaskan@gmail.com>
2025-11-20 15:53:29 +0300

    Update PositionLocation.h

--- Lab2/BookClasses/PositionLocation.h
+++ Lab2/BookClasses/PositionLocation.h
@@ -1,36 +1,13 @@
-/**
- * @file PositionLocation.h
- * @brief Класс для работы с позицией на полке
- */
-
 #ifndef POSITIONLOCATION_H
 #define POSITIONLOCATION_H
-
- /**
-  * @class PositionLocation
-  * @brief Представляет позицию книги на полке
-  */
 class PositionLocation {
 private:
-    int position; ///< Позиция на полке
-
+    int position; 
 public:
-    /**
-     * @brief Конструктор с параметром
-     * @param pos Позиция на полке
-     */
     PositionLocation(int pos);
-
-    /**
-     * @brief Конструктор по умолчанию
-     */
     PositionLocation();
-
-    /**
-     * @brief Получить позицию на полке
-     * @return Позиция на полке
-     */
     int get_position();
 };
+
 
 #endif
