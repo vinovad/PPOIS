d89892a2b8cfa0b34ed9d376aab17cf64c7bb42a
vinovad <advaskan@gmail.com>
2025-11-20 15:49:20 +0300

    Update NumberOfBooks.h

--- Lab2/BookClasses/NumberOfBooks.h
+++ Lab2/BookClasses/NumberOfBooks.h
@@ -1,35 +1,12 @@
-/**
- * @file NumberOfBooks.h
- * @brief Класс для работы с количеством книг
- */
-
 #ifndef NUMBEROFBOOKS_H
 #define NUMBEROFBOOKS_H
-
- /**
-  * @class NumberOfBooks
-  * @brief Представляет количество доступных книг
-  */
 class NumberOfBooks {
 private:
-    int quantity; ///< Количество книг
+    int quantity; 
 
 public:
-    /**
-     * @brief Конструктор с параметром
-     * @param qty Количество книг
-     */
     NumberOfBooks(int qty);
-
-    /**
-     * @brief Конструктор по умолчанию
-     */
     NumberOfBooks();
-
-    /**
-     * @brief Получить количество книг
-     * @return Количество книг
-     */
     int get_quantity();
 };
 
