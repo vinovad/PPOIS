cc744e461c12499340e08ef49878f7112da51026
vinovad <advaskan@gmail.com>
2025-11-20 15:20:57 +0300

    Update Audiobook.h

--- Lab2/BookClasses/Audiobook.h
+++ Lab2/BookClasses/Audiobook.h
@@ -1,36 +1,13 @@
-/**
- * @file Audiobook.h
- * @brief Класс для работы с аудиокнигами
- */
-
 #ifndef AUDIOBOOK_H
 #define AUDIOBOOK_H
-
- /**
-  * @class Audiobook
-  * @brief Представляет информацию о наличии аудиоверсии книги
-  */
 class Audiobook {
 private:
-    bool has_audiobook; ///< Наличие аудиокниги
-
+    bool has_audiobook; 
 public:
-    /**
-     * @brief Конструктор с параметром
-     * @param audio Наличие аудиокниги
-     */
-    Audiobook(bool audio);
-
-    /**
-     * @brief Конструктор по умолчанию
-     */
+    Audiobook(bool audio)
     Audiobook();
-
-    /**
-     * @brief Проверить наличие аудиоверсии
-     * @return true если аудиокнига доступна, false в противном случае
-     */
     bool has_audiobook_version();
 };
+
 
 #endif
