cfb81ca44c3aa9ad7811bf0da92523f7ab2dd891
vinovad <advaskan@gmail.com>
2025-11-20 16:05:37 +0300

    Update WorkExperience.h

--- Lab2/BookClasses/WorkExperience.h
+++ Lab2/BookClasses/WorkExperience.h
@@ -1,41 +1,19 @@
-/**
- * @file WorkExperience.h
- * @brief Класс для работы с опытом работы
- */
-
 #ifndef WORKEXPERIENCE_H
 #define WORKEXPERIENCE_H
 
 #include <string>
 using namespace std;
-
-/**
- * @class WorkExperience
- * @brief Представляет опыт работы сотрудника
- */
 class WorkExperience {
 private:
-    int years_of_experience;    ///< Количество лет опыта
-    string previous_companies;  ///< Предыдущие компании
-    string specialization;      ///< Специализация
+    int years_of_experience;    
+    string previous_companies;
+    string specialization;    
 
 public:
-    /**
-     * @brief Конструктор с параметром
-     * @param experience Количество лет опыта
-     */
-    WorkExperience(int experience);
-
-    /**
-     * @brief Конструктор по умолчанию
-     */
+    WorkExperience(int experience);=
     WorkExperience();
-
-    /**
-     * @brief Получить количество лет опыта
-     * @return Количество лет опыта
-     */
     int get_years_of_experience() const;
 };
+
 
 #endif
