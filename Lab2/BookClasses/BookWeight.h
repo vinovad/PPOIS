41ebdc950dc27c9e9c749053772607a321ed57cf
vinovad <advaskan@gmail.com>
2025-11-20 15:34:07 +0300

    Update BookWeight.h

--- Lab2/BookClasses/BookWeight.h
+++ Lab2/BookClasses/BookWeight.h
@@ -1,48 +1,19 @@
-/**
- * @file BookWeight.h
- * @brief Класс для работы с весом книги
- */
-
 #ifndef BOOKWEIGHT_H
 #define BOOKWEIGHT_H
-
 #include "BookFormat.h"
-
- /**
-  * @class BookWeight
-  * @brief Представляет вес книги и связанную информацию
-  */
 class BookWeight {
 private:
-    double weight;              ///< Вес книги
-    double shipping_weight;     ///< Вес для доставки
-    string weight_category;     ///< Категория веса
-    BookFormat associatedFormat; ///< Ассоциированный формат
+    double weight;           
+    double shipping_weight;    
+    string weight_category;   
+    BookFormat associatedFormat; 
 
 public:
-    /**
-     * @brief Конструктор с параметрами
-     * @param w Вес книги
-     * @param format Ассоциированный формат
-     */
-    BookWeight(double w, const BookFormat& format);
-
-    /**
-     * @brief Конструктор по умолчанию
-     */
+BookWeight(double w, const BookFormat& format);
     BookWeight();
-
-    /**
-     * @brief Получить вес книги
-     * @return Вес книги
-     */
     double get_weight();
-
-    /**
-     * @brief Получить ассоциированный формат
-     * @return Формат книги
-     */
     string get_AssociatedFormat();
 };
+
 
 #endif
