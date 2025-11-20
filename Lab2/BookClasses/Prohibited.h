4ce227186f2a08ba2fd5b9808157701fa830bea5
vinovad <advaskan@gmail.com>
2025-11-20 15:54:55 +0300

    Update Prohibited.h

--- Lab2/BookClasses/Prohibited.h
+++ Lab2/BookClasses/Prohibited.h
@@ -1,46 +1,17 @@
-/**
- * @file Prohibited.h
- * @brief Класс для работы с информацией о запрете книги
- */
-
 #ifndef PROHIBITED_H
 #define PROHIBITED_H
-
 #include "Country.h"
-
- /**
-  * @class Prohibited
-  * @brief Представляет информацию о запрете книги в определенной стране
-  */
 class Prohibited {
 private:
-    bool is_prohibited;        ///< Статус запрета
-    Country associatedCountry; ///< Ассоциированная страна
+    bool is_prohibited; 
+    Country associatedCountry; 
 
 public:
-    /**
-     * @brief Конструктор с параметрами
-     * @param prohibited Статус запрета
-     * @param country Ассоциированная страна
-     */
     Prohibited(bool prohibited, const Country& country);
-
-    /**
-     * @brief Конструктор по умолчанию
-     */
     Prohibited();
-
-    /**
-     * @brief Получить статус запрета
-     * @return true если книга запрещена, false в противном случае
-     */
     bool get_is_prohibited();
-
-    /**
-     * @brief Получить ассоциированную страну
-     * @return Название страны
-     */
     string get_AssociatedCountry();
 };
+
 
 #endif
