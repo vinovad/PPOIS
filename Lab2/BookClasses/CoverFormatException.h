e8918f26ae6ae23891a186c645af9e35ff62f0db
vinovad <advaskan@gmail.com>
2025-11-20 15:34:52 +0300

    Update CoverFormatException.h

--- Lab2/BookClasses/CoverFormatException.h
+++ Lab2/BookClasses/CoverFormatException.h
@@ -1,11 +1,5 @@
-/**
- * @file CoverFormatException.h
- * @brief Исключение для работы с форматами обложки
- */
-
 #ifndef COVER_FORMAT_EXCEPTION_H
 #define COVER_FORMAT_EXCEPTION_H
-
 #include <stdexcept>
 #include <string>
 using namespace std;
@@ -14,5 +8,6 @@ class CoverFormatException : public runtime_error {
 public:
     CoverFormatException(const string& msg) : runtime_error(msg) {}
 };
+
 
 #endif
