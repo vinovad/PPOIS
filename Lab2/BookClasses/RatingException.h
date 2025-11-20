c5d400150db968d023c749b314001542b31ef9d1
vinovad <advaskan@gmail.com>
2025-11-20 15:57:35 +0300

    Update RatingException.h

--- Lab2/BookClasses/RatingException.h
+++ Lab2/BookClasses/RatingException.h
@@ -1,11 +1,5 @@
-/**
- * @file RatingException.h
- * @brief Исключение для работы с рейтингами
- */
-
 #ifndef RATING_EXCEPTION_H
 #define RATING_EXCEPTION_H
-
 #include <stdexcept>
 #include <string>
 using namespace std;
@@ -14,5 +8,6 @@ class RatingException : public runtime_error {
 public:
     RatingException(const string& msg) : runtime_error(msg) {}
 };
+
 
 #endif
