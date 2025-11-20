678d1e31c0d2131ea82d2c851379e0963396483f
vinovad <advaskan@gmail.com>
2025-11-20 15:31:52 +0300

    Update BookSeries.h

--- Lab2/BookClasses/BookSeries.h
+++ Lab2/BookClasses/BookSeries.h
@@ -1,40 +1,17 @@
-/**
- * @file BookSeries.h
- * @brief Класс для работы с сериями книг
- */
-
 #ifndef BOOKSERIES_H
 #define BOOKSERIES_H
-
 #include <string>
 using namespace std;
-
-/**
- * @class BookSeries
- * @brief Представляет информацию о серии книг
- */
 class BookSeries {
 private:
-    string series_name;     ///< Название серии
-    int volume_number;      ///< Номер тома в серии
+    string series_name;   
+    int volume_number;   
 
 public:
-    /**
-     * @brief Конструктор с параметром
-     * @param name Название серии
-     */
     BookSeries(const string& name);
-
-    /**
-     * @brief Конструктор по умолчанию
-     */
     BookSeries();
-
-    /**
-     * @brief Получить название серии
-     * @return Название серии
-     */
     string get_series_name();
 };
+
 
 #endif
