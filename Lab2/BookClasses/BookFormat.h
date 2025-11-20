3535db30f48450500766f46e33dd6096e31d28cb
vinovad <advaskan@gmail.com>
2025-11-20 15:27:03 +0300

    Update BookFormat.h

--- Lab2/BookClasses/BookFormat.h
+++ Lab2/BookClasses/BookFormat.h
@@ -1,48 +1,20 @@
-/**
- * @file BookFormat.h
- * @brief Класс для работы с форматом книги
- */
-
 #ifndef BOOKFORMAT_H
 #define BOOKFORMAT_H
 
 #include "PageCount.h"
 #include <string>
 using namespace std;
-
-/**
- * @class BookFormat
- * @brief Представляет формат книги с информацией о страницах
- */
 class BookFormat {
 private:
-    string bookformat;          ///< Формат книги
-    PageCount associatedPageCount; ///< Ассоциированная информация о страницах
+    string bookformat;        
+    PageCount associatedPageCount; 
 
 public:
-    /**
-     * @brief Конструктор с параметрами
-     * @param bf Формат книги
-     * @param pageCount Ассоциированная информация о страницах
-     */
     BookFormat(const string& bf, const PageCount& pageCount);
-
-    /**
-     * @brief Конструктор по умолчанию
-     */
     BookFormat();
-
-    /**
-     * @brief Получить формат книги
-     * @return Формат книги
-     */
     string get_bookformat();
-
-    /**
-     * @brief Получить количество ассоциированных страниц
-     * @return Количество страниц
-     */
     int get_AssociatedPageCount();
 };
+
 
 #endif
