377532f8ea529b7891f665739d0ce32e0ad4728b
vinovad <advaskan@gmail.com>
2025-11-20 15:19:01 +0300

    Update ArrivalDate.h

--- Lab2/BookClasses/ArrivalDate.h
+++ Lab2/BookClasses/ArrivalDate.h
@@ -1,41 +1,19 @@
-/**
- * @file ArrivalDate.h
- * @brief Класс для работы с датами поступления книг
- */
-
 #ifndef ARRIVALDATE_H
 #define ARRIVALDATE_H
 
 #include <string>
 using namespace std;
-
-/**
- * @class ArrivalDate
- * @brief Представляет информацию о датах поступления и пополнения книг
- */
 class ArrivalDate {
 private:
-    string date;            ///< Фактическая дата поступления
-    string expected_date;   ///< Ожидаемая дата поступления
-    string restock_date;    ///< Дата пополнения запасов
+    string date;            
+    string expected_date;  
+    string restock_date;    
 
 public:
-    /**
-     * @brief Конструктор с параметром
-     * @param d Дата поступления
-     */
-    ArrivalDate(const string& d);
-
-    /**
-     * @brief Конструктор по умолчанию
-     */
+ArrivalDate(const string& d);
     ArrivalDate();
-
-    /**
-     * @brief Получить дату поступления
-     * @return Дата поступления
-     */
     string get_arrival_date();
 };
+
 
 #endif
