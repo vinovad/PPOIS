86b75c4fa4ff37a5ea944babb21560929d209a9b
vinovad <advaskan@gmail.com>
2025-11-20 15:44:55 +0300

    Update LanguageException.h

--- Lab2/BookClasses/LanguageException.h
+++ Lab2/BookClasses/LanguageException.h
@@ -1,11 +1,5 @@
-/**
- * @file LanguageException.h
- * @brief Исключение для работы с языками
- */
-
 #ifndef LANGUAGE_EXCEPTION_H
 #define LANGUAGE_EXCEPTION_H
-
 #include <stdexcept>
 #include <string>
 using namespace std;
@@ -14,5 +8,6 @@ class LanguageException : public runtime_error {
 public:
     LanguageException(const string& msg) : runtime_error(msg) {}
 };
+
 
 #endif
